54055168f92b93660d8fbdbbfbde94473c3b56c0
tox-in <habimanat78@gmail.com>
2024-10-17 12:13:32 +0200

    string representation

--- lesson2-DjangoApp/worldtours/asiatoursagency/models.py
+++ lesson2-DjangoApp/worldtours/asiatoursagency/models.py
@@ -8,3 +8,6 @@ class Tour(models.Model):
     number_of_nights = models.IntegerField()
     price = models.IntegerField()
     
+    #This is a string representation of the tours
+    def __str__(self):
+        return(f"ID:{self.id}: From {self.origin_country} To {self.destination_country}, {self.number_of_nights} nights costs ${self.price}")
