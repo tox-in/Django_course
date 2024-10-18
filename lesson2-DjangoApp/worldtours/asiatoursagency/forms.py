879b303faf38a1139e41324478ea62423bddd7ca
tox-in <habimanat78@gmail.com>
2024-10-18 21:14:00 +0200

    working with authentication

--- lesson2-DjangoApp/worldtours/asiatoursagency/forms.py
+++ lesson2-DjangoApp/worldtours/asiatoursagency/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
@@ -7,3 +8,21 @@ class ContactForm(forms.Form):
     
     def send_email(self):
         print(f"Sending email from {self.cleaned_data['email']} with messsage: {self.cleaned_data['message']}")
+        
+        
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    
+    class Meta:
+        model = User
+        fields = ['username','password','password_confirm']
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        # Check if the passwords match
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Passwords don't match!")
+        return cleaned_data
--- lesson2-DjangoApp/worldtours/asiatoursagency/views.py
+++ lesson2-DjangoApp/worldtours/asiatoursagency/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import Tour
 from .forms import ContactForm
+from .forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
     tours = Tour.objects.all()
@@ -25,3 +31,52 @@ def contact_view(request):
 # Define the contact_success_view function to handle the success page
 def contact_success_view(request):
     return render(request, 'forms/contact-success.html')
+
+#
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = User.objects.create_user(username=username, password=password)
+            login(request, user)
+            return redirect('landing')
+        else:
+            form = RegisterForm()
+            return render(request, 'accounts/register.html', {'form':form})
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            next_url = request.POST.get('next') or request.GET.get('next') or 'home'
+            return redirect(next_url)
+        else:
+            error_message = "Invalid Credentials!"
+    return render(request, 'accounts/login.html', {'error':error_message})
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('login')
+    else:
+        return redirect('landing')
+
+# Landing View
+#Using the decorator
+@login_required
+def landing_view(request):
+    return render(request, 'home/landing.html')
+
+# Protected View
+class ProtectedView(LoginRequiredMixin, View):
+    login_url = '/login'
+    #'next' - to redirect URL
+    redirect_field_name = 'redirect_to'
+    
+    def get(self, request):
+        return render(request, 'registration/protected.html')
