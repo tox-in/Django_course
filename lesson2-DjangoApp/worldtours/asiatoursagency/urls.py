687b2041ec7df05847c1d8218306b729e809f1ac
tox-in <habimanat78@gmail.com>
2024-10-18 10:35:54 +0200

    working with forms

--- lesson2-DjangoApp/worldtours/asiatoursagency/urls.py
+++ lesson2-DjangoApp/worldtours/asiatoursagency/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index
+from . import views
 
 # Define a list of urls patterns
 urlpatterns = [
-    path('', index, name="index")
+    path('', views.index, name="index"),
+    path('home/', views.home_view, name='home'),
+    path('contact/', views.contact_view, name='contact'),
+    path('contact/success/', views.contact_success_view, name='contact-success'),
 ]
--- lesson2-DjangoApp/worldtours/asiatoursagency/views.py
+++ lesson2-DjangoApp/worldtours/asiatoursagency/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Tour
+from .forms import ContactForm
 # Create your views here.
 def index(request):
     tours = Tour.objects.all()
     context = {'tours': tours}
     return render(request, 'tours/index.html') #context
+
+def home_view(request):
+    return render(request, 'forms/home.html')
+
+# Define the contact_view function to handle the contact
+def contact_view(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.send_email()
+            return redirect('contact-success')
+    else:
+        form = ContactForm()
+    context = {'form':form}
+    return render(request, 'forms/contact.html', context)
+
+# Define the contact_success_view function to handle the success page
+def contact_success_view(request):
+    return render(request, 'forms/contact-success.html')
